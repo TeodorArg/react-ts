1a205ae18aaa9084b083ad5118baa25b405d854e
Fedor <teodorarg2023@gmail.com>
2024-01-10 00:15:39 -0300

    feat: add gulp tasks, add important style config

---
+++ gulpfile.mjs
@@ -0,0 +1,124 @@
+import gulp from "gulp";
+import debug from "gulp-debug";
+import svgSprite from "gulp-svg-sprite";
+import webp from "gulp-webp";
+import imageminWebp from "imagemin-webp";
+import { deleteSync } from "del";
+import favicons from "gulp-favicons";
+import concatFilenames from "gulp-concat-filenames";
+
+// Create favicon
+gulp.task("favicons", () => {
+  return gulp
+    .src("src/assets/favicon/*.*")
+    .pipe(
+      favicons({
+        appShortName: "Favorite House",
+        appName: "Favorite House Web",
+        description: "Favorite House",
+        lang: "ru-RU",
+        theme_color: "#F4F4F4",
+        background: "#F4F4F4",
+        path: "favicons/",
+        start_url: "/",
+        icons: {
+          appleIcon: true,
+          favicons: ["favicon.ico"],
+          online: false,
+          appleStartup: false,
+          android: true,
+          firefox: false,
+          yandex: false,
+          windows: false,
+          coast: false,
+        },
+      })
+    )
+    .pipe(gulp.dest("public/favicons/"));
+});
+
+// SVG Config
+const configSvgSprite = {
+  mode: {
+    stack: {
+      sprite: "../icons.svg",
+    },
+  },
+};
+
+// SVG-icons
+gulp.task("sprites", () => {
+  return gulp
+    .src("src/assets/icons/*.svg")
+    .pipe(svgSprite(configSvgSprite))
+    .pipe(gulp.dest("src/assets/icons/sprites"))
+    .pipe(
+      debug({
+        title: "Sprites",
+      })
+    );
+});
+
+function fileNameFormatter(filename) {
+  return filename.replace(/(.svg)/g, "") + ",";
+}
+
+const concatFilenamesOptions = {
+  root: "./src/assets/icons/",
+  template: fileNameFormatter,
+};
+
+gulp.task("listOfIcons", () => {
+  return gulp
+    .src("src/assets/icons/*.svg")
+    .pipe(concatFilenames("fileicons.md", concatFilenamesOptions))
+    .pipe(gulp.dest("src/assets/icons/sprites"));
+});
+
+// Clean svg-icon folder
+gulp.task("cleanSvgIcons", (done) => {
+  deleteSync("src/assets/images/icons/*.svg}");
+  done();
+});
+
+// Convert images
+gulp.task("images", () => {
+  return gulp
+    .src("src/assets/images/**/*.{jpeg,jpg,png,!svg,!webp}")
+    .pipe(
+      webp(
+        imageminWebp({
+          lossless: true,
+          quality: 80,
+          alphaQuality: 100,
+        })
+      )
+    )
+    .pipe(gulp.dest("src/assets/images"));
+});
+gulp.task("cleanImages", (done) => {
+  deleteSync("src/assets/images/**/*.{jpeg,jpg,png,!svg,!webp}");
+  done();
+});
+
+// Gulp Watch
+gulp.task("watch", () => {
+  gulp.watch("src/assets/icons/*.svg", gulp.series("sprites"));
+  gulp.watch(
+    ["src/assets/images/**/*.{jpeg,jpg,png,!svg,!webp}"],
+    gulp.series("images", "cleanImages")
+  );
+});
+
+// Convert Images To Webp
+export const imagesBuild = gulp.series("images");
+// Build Favicons
+export const faviconBuild = gulp.series("favicons");
+// Build Icons Sprite
+export const iconsBuild = gulp.series("sprites");
+// Buil Txt List of Icons
+export const iconsList = gulp.series("listOfIcons");
+// Default task
+export const development = gulp.series("sprites", "images", "watch");
+
+export default development;
--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,9 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
-import { useForm, SubmitHandler } from "react-hook-form"
-
-interface IFormInput {
-  firstName: string
-  lastName: string
-  age: number
-}
+import './App.scss';
 
 function App() {
-  const { register, handleSubmit } = useForm<IFormInput>()
-  const onSubmit: SubmitHandler<IFormInput> = (data) => console.log(data)
+
   return (
     <div className="App">
       <header className="App-header">
@@ -27,12 +19,6 @@ function App() {
         >
           Learn React
         </a>
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <input {...register("firstName", { required: true, maxLength: 20 })} />
-          <input {...register("lastName", { pattern: /^[A-Za-z]+$/i })} />
-          <input type="number" {...register("age", { min: 18, max: 99 })} />
-          <input type="submit" />
-        </form>
       </header>
     </div>
   );
