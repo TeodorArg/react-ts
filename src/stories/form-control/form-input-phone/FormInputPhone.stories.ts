4fc3256ec29440d0828939cea1179d9144a70b89
Fedor <teodorarg2023@gmail.com>
2024-01-11 02:37:35 -0300

    fix: reorganized the structure

--- src/stories/form-control/form-input-phone/FormInputPhone.stories.ts
+++ src/stories/form-control/form-input-phone/FormInputPhone.stories.ts
@@ -3,7 +3,7 @@ import type { Meta, StoryObj } from "@storybook/react";
 import { FormInputPhone } from "./FormInputPhone";
 
 const meta = {
-  title: "Form/InputPhone",
+  title: "Form Control/InputPhone",
   component: FormInputPhone,
   parameters: {
     // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
