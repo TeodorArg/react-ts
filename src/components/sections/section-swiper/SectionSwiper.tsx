ef106fe61a8eaf6dde95af31b4ded8fcde8c8e6e
Fedor <teodorarg2023@gmail.com>
2024-01-26 23:51:25 -0300

    feat: add reviews section

--- src/components/sections/section-swiper/SectionSwiper.tsx
+++ src/components/sections/section-swiper/SectionSwiper.tsx
@@ -15,7 +15,6 @@ interface SectionSwiperProps {
 // Houses:
 const housesArray = [
   {
-    id: 1,
     image: "1",
     title: "Проект 1",
     desc: "Каркасный дом 10,0х11 м",
@@ -26,7 +25,6 @@ const housesArray = [
     floors: "Два"
   },
   {
-    id: 2,
     image: "2",
     title: "Проект 2",
     desc: "Каркасный дом 10,0х11 м",
@@ -37,7 +35,6 @@ const housesArray = [
     floors: "Один"
   },
   {
-    id: 3,
     image: "3",
     title: "Проект 3",
     desc: "Каркасный дом 10,0х11 м",
@@ -48,7 +45,6 @@ const housesArray = [
     floors: "Один"
   },
   {
-    id: 4,
     image: "4",
     title: "Проект 4",
     desc: "Каркасный дом 10,0х11 м",
@@ -59,7 +55,6 @@ const housesArray = [
     floors: "Три"
   },
   {
-    id: 5,
     image: "5",
     title: "Проект 5",
     desc: "Каркасный дом 13,2х11 м",
@@ -73,13 +68,42 @@ const housesArray = [
 
 // Reviews:
 const reviewsArray = [
-  {}
+  {
+    image: "1",
+    title: "Дмитрий Молчанов",
+    desc: "Коттедж, который мы построили, не только выглядит прекрасно снаружи, но и обустроен с учётом всех наших потребностей внутри. Мы рады, что выбрали эту компанию для строительства нашего дома. ",
+    date: "21.11.2023"
+  },
+  {
+    image: "2",
+    title: "Мария Гамидова",
+    desc: "Спасибо за качественное исполнение и профессиональный подход к строительству нашего коттеджа. Мы с удовольствием рекомендуем вас всем своим друзьям и знакомым.",
+    date: "21.11.2023"
+  },
+  {
+    image: "3",
+    title: "Анатолий Толмачев",
+    desc: "Коттедж, который мы построили, стал настоящим уютным уголком для нашей семьи. Мы очень довольны тем, как все выполнено качественно и профессионально.",
+    date: "21.11.2023"
+  },
+  {
+    image: "1",
+    title: "Дмитрий Молчанов 2",
+    desc: "So perhaps, you've generated some fancy text, and you're content that you can now copy and paste your fancy text in the comments section of funny cat videos, but perhaps you're wondering how it's even possible to change the font of your text? Is it some sort of hack? Are you copying and pasting an actual font?",
+    date: "21.11.2023"
+  },
+  {
+    image: "2",
+    title: "Мария Гамидова 2",
+    desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.",
+    date: "21.11.2023"
+  },
 ];
 
 export default function SectionSwiper({
     swiperSectionTitlePtOne = "firstPtTitle",
     swiperSectionTitlePtTwo = "",
-    swiperSectionDesc = "swiperSectionDesc",
+    swiperSectionDesc = "",
     isHousesSwiper = true,
   }: SectionSwiperProps ) {
 
@@ -103,9 +127,12 @@ export default function SectionSwiper({
 
       <div className="section__right-side section__right-side--swiper">
 
-        {isHousesSwiper && (
-          <SwiperSlider swiperBtnCatalog={true} swiperSliders={isHousesSwiper ? housesArray : reviewsArray} sliderType={swiperType}/>
-        )}
+        {isHousesSwiper ? (
+            <SwiperSlider swiperBtnCatalog={true} swiperSliders={isHousesSwiper ? housesArray : reviewsArray} sliderType={swiperType}/>
+          ) : (
+            <SwiperSlider swiperBtnCatalog={false} swiperSliders={isHousesSwiper ? housesArray : reviewsArray} sliderType={swiperType}/>
+          )
+        }
       
       </div>
     </Section>
