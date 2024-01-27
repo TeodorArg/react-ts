54030bed057163b0ca4bb6d7efcd9266d9282a23
Fedor <teodorarg2023@gmail.com>
2024-01-27 16:50:07 -0300

    fix: form-range

--- src/stories/components/forms/SelectionForm.tsx
+++ src/stories/components/forms/SelectionForm.tsx
@@ -1,16 +1,9 @@
-import { useForm, Controller, SubmitHandler } from "react-hook-form";
-import FormRange from "../../form-control/form-range/FormRange";
+import FormRange from '../../form-control/form-range/FormRange';
 import FormInput from '../../form-control/form-input/FormInput';
 import Button from '../../ui/button/Button';
 
 import "../../form-control/_form-control.scss";
 import "./_forms.scss";
-
-interface IFormValues {
-  "price": string,
-  "area": string,
-  "technology": any,  
-}
 
 interface SelectionFormProps {
   /**
@@ -21,25 +14,33 @@ interface SelectionFormProps {
 
 export default function SelectionForm({formBtnLabel="formBtnLabel"}: SelectionFormProps) {
 
-  const { register, handleSubmit, control, formState: { errors } } = useForm<IFormValues>()
-
-  const onSubmit: SubmitHandler<IFormValues> = (data) => {
-    alert(JSON.stringify(data))
-  }
-
   return (
-    <form onSubmit={handleSubmit(onSubmit)} className='form__selection'>
-
-      <FormRange inputLabel="Выберите цену" inputUnits="₽" rangeValueMin={0} rangeValueMax={3990000} minRange={0} maxRange={8950000}/>
-      <FormRange inputLabel="Выберите площадь" inputUnits="м2"/>
-
-      <FormInput register={register("technology", { required: "Technology is required" })}
-        inputLabel="Технология строительства"
-        inputPlaceholder='Технология строительства' 
-        aria-invalid={errors.technology ? "true" : "false"}
-        inputError={errors.technology && errors.technology.message}
+    <form className='form__selection'>
+      
+      <FormRange
+        inputLabel="Выберите цену"
+        inputUnits="₽"
+        minRange={0}
+        maxRange={120000}
+        rangeValueMin={17642}
+        rangeValueMax={87095}
       />
-     <Button btnCustomClass="form__button" btnSubmit={true} btnLabel={formBtnLabel} btnFull={true}/>
+
+      <FormRange
+        inputLabel='Выберите площадь'
+        inputUnits='м2'
+        minRange={98}
+        maxRange={1200}
+        rangeValueMin={150}
+        rangeValueMax={923}
+      />
+
+      <FormInput
+        inputLabel="Технология строительства"
+        inputPlaceholder='Технология строительства'
+      />
+
+      <Button btnCustomClass="form__button" btnSubmit={true} btnLabel={formBtnLabel} btnFull={true}/>
     </form>
   );
 };
--- src/stories/form-control/form-range/FormRange.tsx
+++ src/stories/form-control/form-range/FormRange.tsx
@@ -31,6 +31,10 @@ interface FormRangeProps {
   */
   maxRange?: number,
   /**
+  * defaultValue of Range 
+  */
+  defaultValue?: number[] | string[];
+  /**
   * Input Error any:
   */
   inputError?: any,
@@ -43,15 +47,16 @@ interface FormRangeProps {
 export default function FormRange({
     inputLabel="LabelFormRange",
     inputUnits="pc.",
-    rangeValueMin = 30,
-    rangeValueMax = 60,
     minRange=0,
-    maxRange=100,
+    maxRange=1200,
+    rangeValueMin=345,
+    rangeValueMax=896,
+    defaultValue,
     inputError,
     register,
-    ...props
   } : FormRangeProps) {
     const [value, setValue] = useState([rangeValueMin, rangeValueMax]);
+    console.log(value, value[0], value[1]);
     return (
       <div className="form__group form__range">
         {inputLabel !== "" && (
@@ -59,14 +64,22 @@ export default function FormRange({
         )}
 
         <div className="form__group--inputs">
-          <input className="form__group--item form__input small" value={value[0]} onChange={e => setValue([e.target.value, value[1]])}></input>
-          <input className="form__group--item form__input small" value={value[1]} onChange={e => setValue([value[0], e.target.value, ])}></input>
+
+          <input className="form__group--item form__input small" 
+            value={value[0]} 
+            onChange={e => setValue([e.target.value, value[1]])} />
+
+          <input className="form__group--item form__input small" 
+            value={value[1]} 
+            onChange={e => setValue([value[0], e.target.value, ])}/>
+
         </div>
 
         <div className="form__range--slider">
           <RangeSlider value={value} onInput={setValue} min={minRange} max={maxRange} className="form__group--slider border--default"/>
-          <span>{value[0]}</span>
-          <span>{value[1]}</span>
+          <span>{minRange}</span>
+          <span className="form__range--slider--total">{value[0]} - {value[1]} {inputUnits}</span>
+          <span>{maxRange}</span>
         </div>
        
       </div>
